362a02562248ab5577e1f2bad2fb3d1b8e615857
Emilio Norrmann <enorrmann@santafe.gov.ar>
2019-02-21 13:46:22 -0300

    promises

--- lbc/app.js
+++ lbc/app.js
@@ -1,9 +1,52 @@
+const express = require('express')
+const app = express()
+
+const lightningPayReq = require('bolt11')
+
 const { spawn } = require('child_process');
-const child = spawn('python',["test.py", "L9x2z11vs8LuBzbuyPuz2m"]);
+
+
+var chechInvoice = function(invoice_id) {
+	return new Promise(function(success, nosuccess) {
+
+	    const { spawn } = require('child_process');
+	   const pyprog = spawn('python',["test.py", invoice_id]);
+
+	    pyprog.stdout.on('data', function(data) {
+
+		success(data);
+	    });
+
+	    pyprog.stderr.on('data', (data) => {
+
+		nosuccess(data);
+	    });
+	});
+};
+
+
+
+const checkInvoiceState = function(invoice_id){
+	const child = spawn('python',["test.py", invoice_id]);
 
 child.stdout.on('data', (data) => {
 	const response = JSON.parse(data.toString());
 	const invoice = response.data.invoice;
-	console.log(invoice);
+	return invoice;
+
+/*	const decoded = lightningPayReq.decode(invoice.description);
+	console.log(decoded);*/
 });
 
+};
+
+
+app.get('/api/invoice/:id', (req, res) => {
+
+     chechInvoice(req.params.id).then(function(fromRunpy) {
+        res.end(fromRunpy);
+    });
+
+})
+
+app.listen(4000, () => console.log('Application listening on port 4000!'))
