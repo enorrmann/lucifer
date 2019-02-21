659eb282672db0a83f0d379ca6040cf34034d9c3
Emilio Norrmann <enorrmann@santafe.gov.ar>
2019-02-21 13:11:56 -0300

    toString

--- lbc/app.js
+++ lbc/app.js
@@ -2,7 +2,7 @@ const { spawn } = require('child_process');
 const child = spawn('python',["test.py", "L9x2z11vs8LuBzbuyPuz2m"]);
 
 child.stdout.on('data', (data) => {
-	const response = JSON.parse(`${data}`);
+	const response = JSON.parse(data.toString());
 	const invoice = response.data.invoice;
 	console.log(invoice);
 });
